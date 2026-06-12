9dc7e37724f74d89ec0df849f97e38754c8a5901
xiaoxiaoxia136 <yilingsheng539@126.com>
2026-06-12 23:26:44 +0800

    Update 𝐓𝐤𝐓𝐮𝐛𝐞.js

--- Hiker/𝐓𝐤𝐓𝐮𝐛𝐞.js
+++ Hiker/𝐓𝐤𝐓𝐮𝐛𝐞.js
@@ -1,5 +1,5 @@
 var TkTube = {
-    version: "2026061202"
+    version: "2026061203"
 };
 
 var lazy = $('#noLoading#').lazyRule(function() {
@@ -7,32 +7,52 @@ var lazy = $('#noLoading#').lazyRule(function() {
     var ua = "Mozilla/5.0 (Linux; Android 15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Mobile Safari/537.36";
 
     function homeOf(url) {
-        var m = (url || "").match(/^(https?:\/\/[^\/]+)/i);
-        return m ? m[1] : "https://tktube.com";
+        url = url || "https://tktube.com";
+        var p = url.indexOf("://");
+        if (p < 0) return "https://tktube.com";
+        var s = url.indexOf("/", p + 3);
+        return s > 0 ? url.substring(0, s) : url;
+    }
+
+    function cutBase(url) {
+        url = url || "";
+        var q = url.indexOf("?");
+        var h = url.indexOf("#");
+        var end = url.length;
+        if (q >= 0 && q < end) end = q;
+        if (h >= 0 && h < end) end = h;
+        url = url.substring(0, end);
+        var s = url.lastIndexOf("/");
+        return s >= 0 ? url.substring(0, s + 1) : homeOf(detailUrl) + "/";
     }
 
     function decodeText(str) {
-        return (str || "")
-            .replace(/\\u002F/ig, "/")
-            .replace(/\\\//g, "/")
-            .replace(/&amp;/g, "&")
-            .replace(/&quot;/g, '"')
-            .replace(/&#34;/g, '"')
-            .replace(/&#39;/g, "'")
-            .replace(/\\x3d/ig, "=")
-            .replace(/\\x26/ig, "&");
+        str = str || "";
+        return str
+            .split("\\u002F").join("/")
+            .split("\\u002f").join("/")
+            .split("\\/").join("/")
+            .split("&amp;").join("&")
+            .split("&quot;").join('"')
+            .split("&#34;").join('"')
+            .split("&#39;").join("'")
+            .split("\\x3d").join("=")
+            .split("\\x3D").join("=")
+            .split("\\x26").join("&");
     }
 
     function normalizeUrl(url, base) {
-        url = decodeText(url || "").trim();
+        url = decodeText(url || "");
+        while (url.indexOf(" ") === 0) url = url.substring(1);
+        while (url.length > 0 && url.charAt(url.length - 1) === " ") url = url.substring(0, url.length - 1);
         if (!url) return "";
         try {
-            if (/%[0-9a-f]{2}/i.test(url)) url = decodeURIComponent(url);
+            if (url.indexOf("%") >= 0) url = decodeURIComponent(url);
         } catch (e) {}
-        if (/^https?:\/\//i.test(url)) return url;
+        if (url.indexOf("http://") === 0 || url.indexOf("https://") === 0) return url;
         if (url.indexOf("//") === 0) return "https:" + url;
         if (url.indexOf("/") === 0) return homeOf(base) + url;
-        return (base || homeOf(detailUrl) + "/").replace(/[?#].*$/, "").replace(/\/[^\/]*$/, "/") + url;
+        return cutBase(base || detailUrl) + url;
     }
 
     function requestHtml(url, ref) {
@@ -47,32 +67,131 @@ var lazy = $('#noLoading#').lazyRule(function() {
         }) || "";
     }
 
+    function skipBlank(text, pos) {
+        while (pos < text.length) {
+            var c = text.charAt(pos);
+            if (c !== " " && c !== "\n" && c !== "\r" && c !== "\t") break;
+            pos++;
+        }
+        return pos;
+    }
+
+    function readValueAfter(text, pos) {
+        pos = skipBlank(text, pos);
+        var c = text.charAt(pos);
+        if (c !== ":" && c !== "=") return "";
+        pos = skipBlank(text, pos + 1);
+        var q = text.charAt(pos);
+        var out = "";
+        var i;
+        if (q === '"' || q === "'") {
+            for (i = pos + 1; i < text.length; i++) {
+                c = text.charAt(i);
+                if (c === "\\") {
+                    if (i + 1 < text.length) {
+                        out += c + text.charAt(i + 1);
+                        i++;
+                    }
+                    continue;
+                }
+                if (c === q) break;
+                out += c;
+            }
+            return decodeText(out);
+        }
+        for (i = pos; i < text.length; i++) {
+            c = text.charAt(i);
+            if (c === ";" || c === "," || c === "\n" || c === "\r" || c === "\t" || c === "<" || c === ">") break;
+            out += c;
+        }
+        return decodeText(out);
+    }
+
     function getValue(text, key) {
         text = decodeText(text || "");
-        var safeKey = key;
-        var list = [
-            新建 RegExp("['\"]?" + safeKey + "['\"]?\\s*[:=]\\s*(['\"])([\\s\\S]*?)\\1", "i"),
-            新建 RegExp("flashvars\\." + safeKey + "\\s*=\\s*(['\"])([\\s\\S]*?)\\1", "i"),
-            新建 RegExp("flashvars\\[['\"]" + safeKey + "['\"]\\]\\s*=\\s*(['\"])([\\s\\S]*?)\\1", "i")
+        var tokens = [
+            "flashvars." + key,
+            "flashvars['" + key + "']",
+            'flashvars["' + key + '"]',
+            "'" + key + "'",
+            '"' + key + '"',
+            key
         ];
-        for (var i = 0; i < list.length; i++) {
-            var m = text.match(list[i]);
-            if (m && m[2]) return decodeText(m[2]);
+        for (var t = 0; t < tokens.length; t++) {
+            var token = tokens[t];
+            var start = 0;
+            while (start >= 0) {
+                var p = text.indexOf(token, start);
+                if (p < 0) break;
+                var val = readValueAfter(text, p + token.length);
+                if (val) return val;
+                start = p + token.length;
+            }
         }
         return "";
     }
 
     function isPlayable(url) {
-        return /\.(m3u8|mp4)(?:[?#]|$)/i.test(url) || /\/get_file\//i.test(url);
+        url = (url || "").toLowerCase();
+        return url.indexOf(".m3u8") >= 0 || url.indexOf(".mp4") >= 0 || url.indexOf("/get_file/") >= 0;
     }
 
     function isBadMedia(url) {
-        return /(?:preview|trailer|teaser|thumb|screenshot|videos_screenshots|\/pv\.|_preview\.mp4)/i.test(url || "");
+        url = (url || "").toLowerCase();
+        return url.indexOf("preview") >= 0 ||
+            url.indexOf("trailer") >= 0 ||
+            url.indexOf("teaser") >= 0 ||
+            url.indexOf("thumb") >= 0 ||
+            url.indexOf("screenshot") >= 0 ||
+            url.indexOf("videos_screenshots") >= 0 ||
+            url.indexOf("_preview.mp4") >= 0;
     }
 
     function withHeader(url) {
-        var origin = homeOf(detailUrl);
-        return url + "#isVideo=true#;{Referer@" + detailUrl + "&&Origin@" + origin + "&&User-Agent@" + ua + "}";
+        return url + "#isVideo=true#;{Referer@" + detailUrl + "&&Origin@" + homeOf(detailUrl) + "&&User-Agent@" + ua + "}";
+    }
+
+    function addMedia(url, name, urls, names, rnd) {
+        url = normalizeUrl(url, detailUrl);
+        if (!url || !isPlayable(url) || isBadMedia(url) || urls.indexOf(url) >= 0) return;
+        if (rnd && url.indexOf("rnd=") < 0 && url.indexOf("/get_file/") >= 0) {
+            url += (url.indexOf("?") >= 0 ? "&" : "?") + "rnd=" + rnd;
+        }
+        urls.push(url);
+        names.push(name || "播放");
+    }
+
+    function readUntilDelimiter(text, pos) {
+        var out = "";
+        for (var i = pos; i < text.length; i++) {
+            var c = text.charAt(i);
+            if (c === '"' || c === "'" || c === "<" || c === ">" || c === " " || c === "\n" || c === "\r" || c === "\t") break;
+            out += c;
+        }
+        return out;
+    }
+
+    function scanHttpMedia(text, urls, names, rnd) {
+        var start = 0;
+        while (start >= 0) {
+            var p1 = text.indexOf("http://", start);
+            var p2 = text.indexOf("https://", start);
+            var p = p1 >= 0 && (p2 < 0 || p1 < p2) ? p1 : p2;
+            if (p < 0) break;
+            var u = readUntilDelimiter(text, p);
+            if (isPlayable(u)) addMedia(u, "直连", urls, names, rnd);
+            start = p + 8;
+        }
+    }
+
+    function scanRelativeGetFile(text, urls, names, rnd) {
+        var start = 0;
+        while (start >= 0) {
+            var p = text.indexOf("/get_file/", start);
+            if (p < 0) break;
+            addMedia(readUntilDelimiter(text, p), "直连", urls, names, rnd);
+            start = p + 10;
+        }
     }
 
     try {
@@ -82,33 +201,19 @@ var lazy = $('#noLoading#').lazyRule(function() {
         var rnd = getValue(source, "rnd");
         var urls = [];
         var names = [];
-
-        function 添加(url, 名字) {
-            url = normalizeUrl(url, detailUrl);
-            if (!url || !isPlayable(url) || isBadMedia(url) || urls.indexOf(url) >= 0) return;
-            if (rnd && url.indexOf("rnd=") < 0 && /\/get_file\//i.test(url)) {
-                url += (url.indexOf("?") >= 0 ? "&" : "?") + "rnd=" + rnd;
-            }
-            urls.push(url);
-            names.push(名字 || "播放");
-        }
-
         var keys = ["video_alt_url5", "video_alt_url4", "video_alt_url3", "video_alt_url2", "video_alt_url", "video_url"];
+
         for (var k = 0; k < keys.length; k++) {
             var key = keys[k];
-            var url = getValue(source, key);
-            var 名字 = getValue(source, key + "_text") || key.replace("video_", "").replace(/_/g, " ");
-            添加(url, 名字);
+            var u = getValue(source, key);
+            var n = getValue(source, key + "_text") || key.分屏("video_").join("").分屏("_").join(" ");
+            addMedia(u, n, urls, names, rnd);
         }
 
-        var directList = source.match(/https?:\/\/[^"'<>\\\s]+?(?:\.m3u8|\.mp4|\/get_file\/[^"'<>\\\s]*)(?:\?[^"'<>\\\s]*)?/ig) || [];
-        for (var i = 0; i < directList.length; i++) 添加(directList[i], "直连");
-        var fileList = source.match(/["'](\/get_file\/[^"']+)["']/ig) || [];
-        for (var j = 0; j < fileList.length; j++) 添加(fileList[j].replace(/^["']|["']$/g, ""), "直连");
-        var mediaList = source.match(/["'](\/[^"']+?\.(?:m3u8|mp4)(?:\?[^"']*)?)["']/ig) || [];
-        for (var n = 0; n < mediaList.length; n++) 添加(mediaList[n].replace(/^["']|["']$/g, ""), "直连");
+        scanHttpMedia(source, urls, names, rnd);
+        scanRelativeGetFile(source, urls, names, rnd);
 
-        if (urls.length === 0) return "toast://未找到播放地址";
+        if (urls.length === 0) return "toast://\u672a\u627e\u5230\u64ad\u653e\u5730\u5740";
         if (urls.length === 1) return withHeader(urls[0]);
         var playUrls = [];
         for (var p = 0; p < urls.length; p++) playUrls.push(withHeader(urls[p]));
@@ -117,6 +222,6 @@ var lazy = $('#noLoading#').lazyRule(function() {
             names: names
         };
     } catch (e) {
-        return "toast://解析失败：" + (e.message || e);
+        return "toast://\u89e3\u6790\u5931\u8d25\uff1a" + (e.message || e);
     }
 });
